ff4b73c93237a38c5237ee8cbb98b908f376e4f4
Hozefa Selim <102323503+HozefaSelim@users.noreply.github.com>
2025-02-27 20:41:27 +0300

    fix api url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 function App() {
   return (
     <>
