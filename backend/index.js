e219ef616c70b70a2c164e73d048d2a11ff1d5c6
Hozefa Selim <102323503+HozefaSelim@users.noreply.github.com>
2025-02-27 20:55:11 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,9 @@ const connect = async () => {
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
+app.use("/", (req, res) => {
+  res.json({ message: "Test route is working!" });
+});
 app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/auth", authRoute);
